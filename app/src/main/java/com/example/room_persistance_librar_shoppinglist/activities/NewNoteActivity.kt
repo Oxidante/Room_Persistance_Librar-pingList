d13cee685eccc3aeecf6f0d8825cd96b575028b6
Stepan Kirsanov <oxidantered@gmail.com>
2023-06-19 22:40:49 +0300

    //

--- app/src/main/java/com/example/room_persistance_librar_shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/example/room_persistance_librar_shoppinglist/activities/NewNoteActivity.kt
@@ -1,14 +1,19 @@
 package com.example.room_persistance_librar_shoppinglist.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.room_persistance_librar_shoppinglist.R
 import com.example.room_persistance_librar_shoppinglist.databinding.ActivityNewNoteBinding
 
 class NewNoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewNoteBinding
+    private lateinit var editLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +34,14 @@ class NewNoteActivity : AppCompatActivity() {
 
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun onEditResult(){
+        editLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == Activity.RESULT_OK){
+                log.d
+            }
+        }
     }
 
     private fun actionBarSettings(){
