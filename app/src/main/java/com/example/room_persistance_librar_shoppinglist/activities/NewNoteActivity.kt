ce78775652d5b70c66ae18c3be3101c3ba2a481b
Stepan Kirsanov <oxidantered@gmail.com>
2023-06-21 15:11:55 +0300

    Создание заметок

--- app/src/main/java/com/example/room_persistance_librar_shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/example/room_persistance_librar_shoppinglist/activities/NewNoteActivity.kt
@@ -4,12 +4,19 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Note
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.room_persistance_librar_shoppinglist.R
+import com.example.room_persistance_librar_shoppinglist.database.NoteAdapter
 import com.example.room_persistance_librar_shoppinglist.databinding.ActivityNewNoteBinding
+import com.example.room_persistance_librar_shoppinglist.entities.NoteItem
+import com.example.room_persistance_librar_shoppinglist.fragments.NoteFragment
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class NewNoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewNoteBinding
@@ -28,23 +35,36 @@ class NewNoteActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.id_save){
-
-        } else if (item.itemId == android.R.id.home){
-
+        if (item.itemId == R.id.id_save) {
+            setMainResult()
+        } else if (item.itemId == android.R.id.home) {
+            finish()
         }
         return super.onOptionsItemSelected(item)
     }
 
-    private fun onEditResult(){
-        editLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if (it.resultCode == Activity.RESULT_OK){
-                log.d
-            }
+    private fun setMainResult() {
+        val i = Intent().apply {
+            putExtra(NoteFragment.NEW_NOTE_KEY, createsNewNotes())
         }
     }
 
-    private fun actionBarSettings(){
+    private fun createsNewNotes(): NoteItem {
+        return NoteItem(
+            null,
+            binding.edTitle.text.toString(),
+            binding.edDescription.text.toString(),
+            getCurrentTime(),
+            ""
+        )
+    }
+
+    private fun getCurrentTime(): String {
+        val formatter = SimpleDateFormat("hh:mm:ss - yyyy/MM/dd", Locale.getDefault())
+        return formatter.format(Calendar.getInstance().time)
+    }
+
+    private fun actionBarSettings() {
         val backButton = supportActionBar
         backButton?.setDisplayHomeAsUpEnabled(true)
     }
--- app/src/main/java/com/example/room_persistance_librar_shoppinglist/fragments/NoteFragment.kt
+++ app/src/main/java/com/example/room_persistance_librar_shoppinglist/fragments/NoteFragment.kt
@@ -1,25 +1,36 @@
 package com.example.room_persistance_librar_shoppinglist.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.ContactsContract.CommonDataKinds.Note
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
-import com.example.room_persistance_librar_shoppinglist.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.room_persistance_librar_shoppinglist.activities.MainApp
+import com.example.room_persistance_librar_shoppinglist.activities.NewNoteActivity
 import com.example.room_persistance_librar_shoppinglist.database.MainViewModel
+import com.example.room_persistance_librar_shoppinglist.database.NoteAdapter
+import com.example.room_persistance_librar_shoppinglist.databinding.ActivityNewNoteBinding
 import com.example.room_persistance_librar_shoppinglist.databinding.FragmentNoteBinding
+import com.example.room_persistance_librar_shoppinglist.entities.NoteItem
 
 
 class NoteFragment : BaseFragment() {
     private lateinit var binding: FragmentNoteBinding
+    private lateinit var editLauncher: ActivityResultLauncher<Intent>
+    private lateinit var adapter: NoteAdapter
+
     private val mainViewModel: MainViewModel by activityViewModels {
         MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
     }
 
     override fun onClickNew() {
-        TODO("Not yet implemented")
+        editLauncher.launch(Intent(activity, NewNoteActivity::class.java))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +40,39 @@ class NoteFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentNoteBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+        observer()
+    }
+
+    private fun initRcView() = with(binding) {
+        rcViewNote.layoutManager = LinearLayoutManager(activity)
+        adapter = NoteAdapter()
+        rcViewNote.adapter = adapter
+    }
+
+    private fun observer() {
+        mainViewModel.allNotes.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
+    }
+
+    private fun onEditResult() {
+        editLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                mainViewModel.insertNote(it.data?.getSerializableExtra(NEW_NOTE_KEY) as NoteItem)
+            }
+        }
+    }
+
     companion object {
+        const val NEW_NOTE_KEY = "new_note_key"
 
         @JvmStatic
         fun newInstance() = NoteFragment()
